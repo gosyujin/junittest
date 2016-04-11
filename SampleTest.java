38bb5531716b1d40552aa2e674e69695c1ce14d0
kk_Ataka <kk_ataka@ring.skr.jp>
2016-04-11 13:20:35 +0900

    fc

---
+++ SampleTest.java
@@ -0,0 +1,13 @@
+package jp.utils;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SampleTest {
+        @Test
+        public void 正常publicにnull接続falseできるtrueこと() throws Exception {
+            assertThat(true, is(Boolean.TRUE));
+        }
+}
